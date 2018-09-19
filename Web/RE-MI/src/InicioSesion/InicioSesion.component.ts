6123260b32a8b84dafa4f89406079ca3e2faa507
201710057010 <30273619+201710057010@users.noreply.github.com>
2018-09-18 20:47:14 -0500

    login comienzo
    primeras adiciones a la pagina del login

--- Web/RE-MI/src/InicioSesion/InicioSesion.component.ts
+++ Web/RE-MI/src/InicioSesion/InicioSesion.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './inicioSesion.component.html',
+  styleUrls: ['./inicioSesion.component.css']
 })
 export class AppComponent {
   title = 'RE-MI';
